2f7cefb035ffa7a3709b31a20ed997d39e13824b
ITPS <ITPS@DESKTOP-N99JP2B>
2023-09-20 16:35:02 +0900

    add

--- tut06/src/tut06/UsersEx.java
+++ tut06/src/tut06/UsersEx.java
@@ -9,58 +9,68 @@ class Users {
 	//생성자 : 객체 초기화 -> public 으로 시작하고 / 클래스이름과 동일 / 리턴값 x
 	public Users() {}
 	
-	public Users(String userid) {
-		this.userid = userid;
-	}
-	
-	public Users(String userid, String passwd) {
-		this.userid = userid;
-		this.passwd = passwd;
-	}
-
 	public Users(String userid, String passwd, int age) { //오버로딩
 		this.userid = userid;
 		this.passwd = passwd;
 		this.age = age; 
 	}
 	
-	//getter, setter
+	//캡슐화(데이터 보호) 처리 : getter(가져오기), setter(저장하기)
+	public String getUserid() {
+		return userid;
+	}
 
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+
+	public String getPasswd() {
+		return passwd;
+	}
+
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	//메소드
 	@Override
 	public String toString() {
 		return "Users [userid=" + userid + ", passwd=" + passwd + ", age=" + age + "]";
-	}
-	
-	
-	
+	}	
 	
 }
 public class UsersEx {
 
 	public static void main(String[] args) {
-		//회원가입 1명을 처리하시오.
-		//생성자를 이용해서 저장
-		//getter, setter 
 		
-		Users[] user = new Users[3]; //회원 한명 저장(아이디, 비밀번호, 나이 한꺼번에 저장) \ '객체'로 선언한 배열 => 타입이 다른 필드 3개를 하나로 묶어서 처리
-		for(int i = 0; i < user.length; ++i) {
+		//3명의 회원(Users)을 생성자로 저장 후 출력
+		Users[] user = new Users[3]; //3명 -> 초기화 3번 해야함 
+
+		for(int i = 0; i < 3; i++) {
+			user[i] = new Users("아이디", "비번", 0); //초기화
+			System.out.println(user[i].toString());
+		}	
+		
+		System.out.println("============================");
+		
+		for(int i = 0; i < 3; i++) {
+			//set 이용해서 저장 
 			user[i] = new Users();
-			/*user[i] = new Users("아이디");
-			  user[i] = new Users("아이디", "비번");
-			  user[i] = new Users("아이디", "비번", 10);*/
+			user[i].setUserid("Busan");
+			user[i].setPasswd("1234");
+			user[i].setAge(10);
+			
+			System.out.println(user[i].toString()); //출력
+
 		}
-		
-		//생성자를 이용해서 초기화(값 입력)
-		user[0] = new Users("아이디1", "비번1", 10);
-		user[1] = new Users("아이디2", "비번2", 20);
-		user[2] = new Users("아이디3", "비번3", 30);
-
-		System.out.println(user[0].toString());
-		System.out.println(user[1].toString());
-		System.out.println(user[2].toString());
-
-
-		
 		
 	}
 
